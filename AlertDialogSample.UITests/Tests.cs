674e35261e04fd97e28d0ee46267b6b371fd3d01
malirezaie <m.alirezaie@gmail.com>
2016-11-28 16:24:01 -0800

    Alert Dialog Sample Commit
    Committing the Alert Dialog backdoor Sample

---
+++ AlertDialogSample.UITests/Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+using Xamarin.UITest.Queries;
+
+namespace AlertDialogSample.UITests
+{
+	[TestFixture]
+	public class Tests
+	{
+		AndroidApp app;
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			app = ConfigureApp.Android.StartApp();
+		}
+
+		[Test]
+		public void ReplTest()
+		{
+			app.Repl();
+		}
+
+		[Test]
+		public void BackdoorTest()
+		{
+			app.Screenshot("Invoking Positive Backdoor");
+
+			app.Invoke("PositiveBackdoor","hello");
+
+			app.Screenshot("Invoking Negative Backdoor");
+
+			app.Invoke("NegativeBackdoor");
+
+		}
+
+
+
+	}
+}
---
+++ MainActivity.cs
@@ -0,0 +1,75 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Java.Interop;
+
+namespace AlertDialogSample
+{
+	[Activity(Label = "AlertDialogSample", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+		TextView view;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			view = FindViewById<TextView>(Resource.Id.myText);
+
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+			button.Click += (s, e) =>
+			{
+				AlertDialog.Builder builder = new AlertDialog.Builder(this);
+				builder.SetTitle("Alert!");
+				builder.SetMessage("Plese click on Yes or No.");
+				builder.SetCancelable(false);
+				builder.SetPositiveButton("OK", (sender, arg) =>
+				{
+					HandlePositiveClicked();
+				});
+
+				builder.SetNegativeButton("Cancel", (sender, arg) =>
+				{
+					HandleNegativeClicked();
+				});
+
+				builder.Show();
+			};
+
+		}
+
+		public void HandlePositiveClicked()
+		{
+			view.Text = "Positive Button Clicked!";
+		}
+
+		public void HandleNegativeClicked()
+		{
+			view.Text = "Negative Button Clicked!";
+		}
+
+		#region BACKDOOR METHODS
+
+		[Export("PositiveBackdoor")]
+		public void PositiveBackdoor(string param1)
+		{
+			HandlePositiveClicked();
+		}
+
+		[Export("NegativeBackdoor")]
+		public void NegativeBackdoor()
+		{
+			HandleNegativeClicked();
+		}
+
+		#endregion
+
+	}
+}
+
